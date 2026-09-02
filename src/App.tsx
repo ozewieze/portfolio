321af9f2cb6bffcbb6a43a78ff3b560afe4459c1
ozewieze <stefballyn@hotmail.com>
2026-09-02 13:04:17 +0200

    Complete Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,8 @@
 import Header from "./components/Header";
 const App = () => {
-  const navItems = [
-    { label: "projects", href: "#projects" },
-    { label: "about", href: "#about" },
-    { label: "skills", href: "#skills" },
-    { label: "contact", href: "#contact" },
-  ];
   return (
     <>
-      <Header navItems={navItems} />
+      <Header />
       <div className="container">
         <h1>Hallo, Steffieboy!</h1>
         <p>Welkom bij je React applicatie.</p>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,22 +1,33 @@
-type NavItems = { label: string; href: string }[];
+type NavItem = { label: string; href: string };
 
-export default function Header({ navItems }: { navItems: NavItems }) {
+const navItems: NavItem[] = [
+  { label: "Projects", href: "#projects" },
+  { label: "About", href: "#about" },
+  { label: "Skills", href: "#skills" },
+  { label: "Contact", href: "#contact" },
+];
+
+export default function Header() {
   return (
-    <header className="header">
-      <div>Stef Ballyn</div>
-      <nav>
-        <ol className="header-navList">
-          {navItems.map((item) => {
-            return (
-              <li key={item.label}>
-                <a className="header-navLink" href={item.href}>
-                  {item.label}
-                </a>
-              </li>
-            );
-          })}
-        </ol>
-      </nav>
+    <header className="header" id="header">
+      <div className="container header__container">
+        <a className="header__name" href="#header">
+          Stef Ballyn
+        </a>
+        <nav>
+          <ul className="header__nav-list">
+            {navItems.map((item) => {
+              return (
+                <li key={item.label}>
+                  <a className="header__nav-link" href={item.href}>
+                    {item.label}
+                  </a>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 }
